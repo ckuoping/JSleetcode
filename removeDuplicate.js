5043ea7f545bec01ad2197163e963c6b6b7533b7
Marvin <ckuoping@gmail.com>
2023-09-10 18:17:43 +0800

    remove duplicate

---
+++ removeDuplicate.js
@@ -0,0 +1,31 @@
+var removeDuplicates = function(nums) {
+    /*
+    let len = 0;
+    let num;
+
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] !== num){
+            nums[len] = nums[i];
+            num = nums[len];
+            len++;
+        }
+    }
+
+    return len;
+    */
+   let num;
+
+   for(let i=0; i<nums.length; i++){
+    if(nums[i] !== num){
+        num = nums[i];
+    }
+    else{
+        nums.splice(nums.indexOf(nums[i]),1);
+        i--;
+    }
+   }
+
+   return nums.length;
+};
+
+console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
