78ca566c0ed1978513567f7db7a33deb33d4fdaa
Marvin <ckuoping@gmail.com>
2023-09-11 23:10:52 +0800

    longest palindrome

--- longestPalindrome.js
+++ longestPalindrome.js
@@ -4,6 +4,7 @@
 // "babad"
 
 // 暴力解
+/*
 var longestPalindrome = function(s) {
     let ans = '';
     let str_num = 0;
@@ -19,10 +20,9 @@ var longestPalindrome = function(s) {
 
     function checkSubstring (str){
         
-        let str_ori = str.split('').join();
         let str_rev = str.split('').reverse().join();
 
-        if(str_ori === str_rev){
+        if(str === str_rev){
             if(str.length > str_num){
                 str_num = str.length;
                 ans = str;
@@ -31,5 +31,43 @@ var longestPalindrome = function(s) {
     }
     return ans;
 };
+*/
+
+// 最佳解
+var longestPalindrome = function(s) {
+    let result = '';
+
+    for(let i=0; i<s.length; i++){
+        let leftIndex = i;
+        let righttIndex = i;
+
+        while(s[leftIndex]!==undefined && s[righttIndex] !== undefined && s[leftIndex] === s[righttIndex]){
+            leftIndex--;
+            righttIndex++;
+        }
+        let str = s.substring(leftIndex+1, righttIndex);
+
+        if(str.length > result.length){
+            result = str;
+        }
+    }
+
+        for(let i=0; i<s.length-1; i++){
+        let leftIndex = i;
+        let righttIndex = i+1;
+
+        while(s[leftIndex]!==undefined && s[righttIndex] !== undefined && s[leftIndex] === s[righttIndex]){
+            leftIndex--;
+            righttIndex++;
+        }
+        let str = s.substring(leftIndex+1, righttIndex);
+
+        if(str.length > result.length){
+            result = str;
+        }
+    }
+
+    return result;
+}
 
 console.log(longestPalindrome("cbbd"))
