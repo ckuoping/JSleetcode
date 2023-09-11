d039cc22aec1bf87dbf74f49ddcf44b2803dc700
Marvin <ckuoping@gmail.com>
2023-09-11 21:30:37 +0800

    複習reverse

--- reverse.js
+++ reverse.js
@@ -1,3 +1,4 @@
+/*
 let reverseString = function(txt){
     txt = txt.split("");
 
@@ -7,6 +8,16 @@ let reverseString = function(txt){
         txt_rev += txt[i]
     }
     return txt_rev;
+}
+*/
+let reverseString = function(txt){
+    let text_rev = '';
+
+    for(let i = txt.length-1; i>=0; i--){
+        text_rev = text_rev + txt[i]
+    }
+
+    return text_rev
 }
 
 console.log(reverseString("hello"))
