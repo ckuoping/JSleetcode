6429db801229e42b8731efe55b1b0c5637338dd1
Marvin <ckuoping@gmail.com>
2023-09-11 21:21:25 +0800

    複習palindrome number

--- PalindromeNumber.js
+++ PalindromeNumber.js
@@ -1,3 +1,4 @@
+/*
 var isPalindrome = function(x) {
    
     let x_ = x.toString().split("").join();
@@ -10,6 +11,19 @@ var isPalindrome = function(x) {
    
    
 };
+*/
+
+var isPalindrome = function(x) {
+   
+    let x_ = x.toString().split("").join("");
+    let y_ = x.toString().split("").reverse().join("");
+    // console.log(x_,y_)
+    if(x_ === y_) return true;
+    else return false;
+
+   
+   
+};
 
 
-console.log(isPalindrome(121))
+console.log(isPalindrome(3456))
