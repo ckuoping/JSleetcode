5c25c690d132010896663f3c8fa8fc4505236c6c
Marvin <ckuoping@gmail.com>
2023-09-12 21:03:57 +0800

    複習plus 1

--- plus1.js
+++ plus1.js
@@ -1,3 +1,4 @@
+/*
 var plusOne = function(digits) {
     // let num = parseInt(digits.join().replaceAll(',',''));
     // let num_plus1_arr = (num + 1).toString().split('');
@@ -32,5 +33,50 @@ var plusOne = function(digits) {
     return digits;
 
 };
+*/
 
-console.log(plusOne([9]))
+var plusOne = function(digits) {
+    let endIndex = digits.length - 1;
+
+    if(digits[endIndex] !== 9){
+        digits[endIndex]++;
+        return digits;
+    }else{
+        while(digits[endIndex] === 9){
+            digits[endIndex] = 0;
+
+            // if(digits[endIndex - 1])
+            // {
+            //     if(digits[endIndex - 1] === 9){
+            //         endIndex--;
+            //         continue;
+            //     }else{
+            //         digits[endIndex - 1]++;
+            //     }
+                
+            // }else{
+            //     digits.unshift(1);
+            // }
+            
+            if(digits[endIndex - 1] !== undefined){
+                if(digits[endIndex - 1] === 9){
+                    endIndex--;
+                    continue;
+                }else{
+                    digits[endIndex - 1]++;
+                }
+            }else{
+                digits.unshift(1);
+            }
+
+        }
+    }
+
+
+    return digits;
+
+};
+
+console.log(plusOne([9]));
+console.log(plusOne([1,2,3]));
+console.log(plusOne([4,3,2,1]));
