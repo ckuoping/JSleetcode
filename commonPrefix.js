a6eb5342c965c1d95ab7b1ccbbbd9f789e49a347
Marvin <ckuoping@gmail.com>
2023-09-12 08:02:08 +0800

    開始longest common prefix

---
+++ commonPrefix.js
@@ -0,0 +1,16 @@
+// strs = ["flower","flow","flight"]  >> "fl"
+// strs = ["dog","racecar","car"]  > ""
+
+var longestCommonPrefix = function(strs) {
+    let result = "";
+
+    for(let i=1; i<strs.length; i++){
+        let id = 0
+        let prefix = strs[0][id];
+
+        if(strs[i].includes(prefix)){
+
+        }
+
+    }
+};
