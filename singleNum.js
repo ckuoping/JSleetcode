ba8899561d242640ba18ebb75b602b3796ed123f
PingChang <pingchang@moldex3d.com>
2023-09-19 11:27:28 +0800

    修改程式

--- singleNum.js
+++ singleNum.js
@@ -1,17 +1,32 @@
 var singleNumber = function(nums) {
-    if(nums.length === 1){
-        return nums[0];
-    }else{
-        let temp = [...nums];
-        for(let i=0; i<nums.length; i++){
-            temp.splice(i,1);
-            if(temp.indexOf(nums[i]) === -1){
-                return nums[i];
-            }else{
-                temp = [...nums];
-            } 
+    let repeating = {};
+
+    for (let i = 0; i < nums.length; i++) {
+        if (!repeating[nums[i]]) {
+            repeating[nums[i]] = true;
+        } else {
+            repeating[nums[i]] = false;
         }
     }
+
+    for (const key in repeating) {
+        if (repeating[key]) {
+            return key
+        }
+    }
+
+
+    // nums = nums.sort();
+
+    // for (let i = 0; i < nums.length; i++) {
+    //     console.log('i', i);
+    //     if (nums[i] === nums[i + 1]) {
+    //         nums.splice(i, 2);
+    //         i--; // 下次i會從index = 1開始，需要改成從0開始
+    //     } else {
+    //         return nums[i];
+    //     }
+    // }
 };
 
-console.log(singleNumber([4,1,2,1,2]));
+console.log(singleNumber([4, 1, 2, 1, 2]));
