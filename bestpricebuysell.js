b207c02e38e7b5d4cd0d3bcea3bf00cfeb155ccb
PingChang <pingchang@moldex3d.com>
2023-09-22 09:59:37 +0800

    修改買進賣出程式

--- bestpricebuysell.js
+++ bestpricebuysell.js
@@ -1,4 +1,6 @@
 var maxProfit = function(prices) {
+    // 方法 1：雙重迴圈
+    /*
     let max_profit = 0;
     for(let i = 0; i<prices.length; i++){
         for(let j = i+1; j<prices.length; j++){
@@ -10,7 +12,30 @@ var maxProfit = function(prices) {
     }
 
     return max_profit;
+    */
+    // 方法 2：
+    let min = Number.MAX_SAFE_INTEGER;
+    let max_profile = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+
+        // 先找出陣列中最低的數值
+        if (prices[i] < min) {
+            min = prices[i];
+        }
+
+        // 再找出陣列中各元素和該min的差異值
+        let diff = prices[i] - min;
+
+        // 若差異大於前一次紀錄的差異，則替換差議值
+        if (diff > max_profile) {
+            max_profile = diff;
+        }
+    }
+
+    return max_profile;
+
 };
 
-console.log(maxProfit([7,1,5,3,6,4]));
-console.log(maxProfit([7,6,4,3,1]));
+console.log(maxProfit([7, 1, 5, 3, 6, 4]));
+console.log(maxProfit([7, 6, 4, 3, 1]));
